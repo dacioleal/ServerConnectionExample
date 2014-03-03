fc0269f37b111cbf7413a30edc3a12da03785bbe
dacioleal <dacioleal@gmail.com>
2014-03-03 12:21:24 +0100

    Initial Commit

---
+++ ServerConnectionExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ServerConnectionExample
+//
+//  Created by Dacio Leal Rodriguez on 03/03/14.
+//  Copyright (c) 2014 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ServerConnectionExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ServerConnectionExample
+//
+//  Created by Dacio Leal Rodriguez on 03/03/14.
+//  Copyright (c) 2014 Dacio Leal Rodriguez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
